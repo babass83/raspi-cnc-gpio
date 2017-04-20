2b351337a7cc1718c2dceeb5db21b0a2fb6c23dd
babass83 <sebastien.rx@gmail.com>
2017-04-20 19:31:27 +0200

    Add systemd script

--- enable_relay.py
+++ enable_relay.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
+import time
+
+pin_number=32
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(6, GPIO.OUT)
+GPIO.setup(pin_number, GPIO.OUT)
 
-GPIO.output(6, GPIO.HIGH)
-
-GPIO.cleanup()
+GPIO.output(pin_number, GPIO.HIGH)
